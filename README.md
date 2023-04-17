1c26e3e5d3272c4198512e814c7394907ac7977b
Alexandre Christian Elie Gagnon <alxgnon@gmail.com>
2023-04-16 21:10:16 -0400

    changes

---
+++ README.md
@@ -0,0 +1,3 @@
+# rhizo-moda
+
+rhizo-moda is great!
---
+++ main.js
@@ -0,0 +1,136 @@
+function setFontSize(editor, fontSize) {
+    editor.setOptions({
+        fontSize: fontSize
+    });
+}
+
+function setModeTheme(editor, theme) {
+    editor.setOptions({
+        mode: "ace/mode/rhizomoda",
+        theme: theme,
+        scrollPastEnd: 1,
+        showLineNumbers: false
+    });
+}
+
+function setWrapColumn(editor, wrapColumn) {
+    editor.setOptions({
+        wrap: wrapColumn,
+        printMargin: wrapColumn
+    });
+}
+
+function setTabSize(editor, tabSize) {
+    editor.setOptions({
+        tabSize: tabSize,
+        useSoftTabs: false
+    });
+}
+
+var exampleDocument = "\nQUESTION: Why?\n" +
+    "NOTE: No reason.\n\n" +
+    "QUESTION: What the hell is a rhizo-moda?\n" +
+    "NOTE: That's the wrong question. The right question is 'what can a rhizo-moda do for you'?\n\n" +
+    "PROJECT: All in a day's work\n" +
+    "DONE: Buy cheese\n" +
+    "TODO: Make friends\n" +
+    "IN PROGRESS: Care for family\n\n" +
+    "TOPIC: Keywords\n" +
+    "SECTION: More keywords :-)\n" +
+    "I.E. \"That is\"\n" +
+    "E.G. \"For example\"\n\n" +
+    "MAYBE...? Dunno.\n" +
+    "TWEAKS: Making small adjustments\n" +
+    "WAITING FOR: Sweet release/...\n\n";
+
+function loadSavedFile(session, documentName) {
+    var value = localStorage.getItem(documentName);
+    if (typeof value == "string") {
+        session.setValue(value);
+        localStorage.setItem(documentName + Date.now(), value);
+    } else {
+        session.setValue(exampleDocument);
+    }
+}
+
+function enableAutoSave(session, documentName) {
+    session.on("change", function () {
+        localStorage.setItem(documentName, session.getValue());
+    });
+}
+
+function disableSaveDialog(e) {
+    if ((e.ctrlKey || e.metaKey) && e.key === "s") {
+        e.preventDefault();
+    }
+}
+
+ace.define("ace/mode/rhizomoda_highlight_rules", ["require", "exports", "module", "ace/lib/oop", "ace/mode/text_highlight_rules"],
+    function (require, exports) {
+        "use strict";
+        var oop = require("../lib/oop");
+        var TextHighlightRules = require("./text_highlight_rules").TextHighlightRules;
+        var RhizomodaHighlightRules = function () {
+            this.$rules = {
+                "start": [{
+                    token: "string",
+                    regex: /(DONE|PROJECT|QUESTION|SECTION|TOPIC)[.!?,:;]+/
+                }, {
+                    token: "keyword",
+                    regex: /(E\.?G|I\.?E|IN PROGRESS|MAYBE|NOTE|TODO|TWEAKS|WAITING FOR)[.!?,:;]+/
+                }, {
+                    token: "keyword.operator",
+                    regex: /(\:\w+\:|\<[\/\\]?3|[\(\)\\\D|\*\$][\-\^]?[\:\;\=]|[\:\;\=B8][\-\^]?[3DOPp\@\$\*\\\)\(\/\|])(?=\s|[\!\.\?]|$)/
+                }]
+            };
+            this.normalizeRules();
+        };
+        RhizomodaHighlightRules.metaData = {
+            fileTypes: ["document"],
+            name: "rhizomoda"
+        };
+        oop.inherits(RhizomodaHighlightRules, TextHighlightRules);
+        exports.RhizomodaHighlightRules = RhizomodaHighlightRules;
+    }
+);
+
+ace.define("ace/mode/rhizomoda", ["require", "exports", "module", "ace/lib/oop", "ace/mode/text", "ace/mode/rhizomoda_highlight_rules"],
+    function (require, exports) {
+        "use strict";
+        var oop = require("../lib/oop");
+        var TextMode = require("./text").Mode;
+        var RhizomodaHighlightRules = require("./rhizomoda_highlight_rules").RhizomodaHighlightRules;
+        var Mode = function () {
+            this.HighlightRules = RhizomodaHighlightRules;
+            this.$behaviour = this.$defaultBehaviour;
+        };
+        oop.inherits(Mode, TextMode);
+        (function () {
+            this.$id = "ace/mode/rhizomoda";
+        }).call(Mode.prototype);
+        exports.Mode = Mode;
+    }
+);
+
+(function () {
+    ace.require(["ace/mode/rhizomoda"], function (m) {
+        if (typeof module == "object" && typeof exports == "object" && module) {
+            module.exports = m;
+        }
+    });
+})();
+
+var editor = ace.edit("myeditor");
+
+setFontSize(editor, 14);
+setModeTheme(editor, "ace/theme/ambiance");
+setWrapColumn(editor, 100);
+setTabSize(editor, 8);
+
+var session = editor.getSession();
+
+var documentName = "rhizo.document";
+
+loadSavedFile(session, documentName);
+enableAutoSave(session, documentName);
+document.addEventListener("keydown", disableSaveDialog);
