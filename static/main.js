06ee2131f7669f09ec6e4fa8c858311081bc63bf
Alexandre Christian Elie Gagnon <alxgnon@gmail.com>
2023-04-19 00:52:54 -0400

    changes

--- static/main.js
+++ static/main.js
@@ -11,14 +11,14 @@ ace.define("ace/mode/rhizomoda",
         function HighlightRules() {
             this.$rules = {
                 "start": [{
-                    token: "string",
-                    regex: /(DONE|PROJECT|QUESTION|SECTION|TOPIC)[.!?,:;]+/
-                }, {
                     token: "keyword",
                     regex: /(E\.?G|I\.?E|IN PROGRESS|MAYBE|NAME|NOTE|TODO|TWEAKS|WAITING FOR)[.!?,:;]+/
                 }, {
                     token: "keyword.operator",
                     regex: /(\:\w+\:|\<[\/\\]?3|[\(\)\\|\*\$][\-\^]?[\:\;\=]|[\:\;\=B8][\-\^]?[3DOPp\@\$\*\\\)\(\/\|])(?=\s|[\!\.\?]|$)/
+                }, {
+                    token: "string",
+                    regex: /(DONE|PROJECT|QUESTION|SECTION|TOPIC)[.!?,:;]+/
                 }]
             };
 
@@ -33,9 +33,9 @@ ace.define("ace/mode/rhizomoda",
         oop.inherits(HighlightRules, TextHighlightRules);
 
         function Mode() {
-            this.HighlightRules = HighlightRules;
             this.$behaviour = this.$defaultBehaviour;
             this.foldingRules = new FoldMode();
+            this.HighlightRules = HighlightRules;
         };
 
         oop.inherits(Mode, TextMode);
@@ -48,101 +48,105 @@ ace.define("ace/mode/rhizomoda",
     }
 );
 
-var root = document.body;
+$(function () {
+    var root = document.body;
 
-m.render(root, [
-    m("div", {
-        class: "whiteboard",
-        id: "whiteboard"
-    }),
-    m("main", [
+    m.render(root, [
         m("div", {
-            class: "editor",
-            id: "myprimary"
+            class: "whiteboard",
+            id: "mywhiteboard"
         }),
-        m("div", {
-            class: "editor",
-            id: "mysecondary"
-        }),
-        m("div", {
-            class: "editor",
-            id: "mytertiary"
-        })
-    ])
-]);
+        m("main", [
+            m("div", {
+                class: "editor",
+                id: "myprimary"
+            }),
+            m("div", {
+                class: "editor",
+                id: "mysecondary"
+            }),
+            m("div", {
+                class: "editor",
+                id: "mytertiary"
+            })
+        ])
+    ]);
 
-var editorOptions = {
-    fontSize: 11,
-    mode: "ace/mode/rhizomoda",
-    printMargin: 34,
-    scrollPastEnd: 1,
-    showLineNumbers: false,
-    tabSize: 2,
-    theme: "ace/theme/ambiance",
-    useSoftTabs: false,
-    wrap: 34
-};
+    var editorOptions = {
+        fontSize: 11,
+        mode: "ace/mode/rhizomoda",
+        printMargin: 34,
+        scrollPastEnd: 1,
+        showLineNumbers: false,
+        tabSize: 2,
+        theme: "ace/theme/ambiance",
+        useSoftTabs: false,
+        wrap: 34
+    };
 
-ace.require(["ace/mode/rhizomoda"]);
+    ace.require(["ace/mode/rhizomoda"]);
 
-var primary = {
-    editor: ace.edit("myprimary", editorOptions),
-    filename: "index.document"
-};
+    var primary = {
+        editor: ace.edit("myprimary", editorOptions),
+        filename: "index.document"
+    };
 
-var secondary = {
-    editor: ace.edit("mysecondary", editorOptions),
-    filename: "moda.document"
-};
+    var secondary = {
+        editor: ace.edit("mysecondary", editorOptions),
+        filename: "moda.document"
+    };
 
-var tertiary = {
-    editor: ace.edit("mytertiary", editorOptions),
-    filename: "rhizo.document"
-};
+    var tertiary = {
+        editor: ace.edit("mytertiary", editorOptions),
+        filename: "rhizo.document"
+    };
 
-hookup(primary);
-hookup(secondary);
-hookup(tertiary);
+    hookup(primary);
+    hookup(secondary);
+    hookup(tertiary);
 
-function hookup(p) {
-    var session = p.editor.getSession();
-    var value = localStorage.getItem(p.filename);
+    function hookup(p) {
+        var session = p.editor.getSession();
+        var value = localStorage.getItem(p.filename);
 
-    if (typeof value == "string") {
-        localStorage.setItem(p.filename + Date.now(), value);
-        session.setValue(value);
+        if (typeof value == "string") {
+            localStorage.setItem(p.filename + Date.now(), value);
+            session.setValue(value);
+        }
+
+        session.on("change", function () {
+            localStorage.setItem(p.filename, session.getValue());
+        });
     }
 
-    session.on("change", function () {
-        localStorage.setItem(p.filename, session.getValue());
+    document.addEventListener("keydown", function (e) {
+        if ((e.ctrlKey || e.metaKey) && e.key === "s") {
+            e.preventDefault();
+        }
     });
-}
 
-document.addEventListener("keydown", function (e) {
-    if ((e.ctrlKey || e.metaKey) && e.key === "s") {
-        e.preventDefault();
-    }
+    var wallpapers = [
+        "fill-071",
+        "fill-074",
+        "fill-075",
+        "fill-076",
+        "fill-080",
+        "fill-087",
+        "fill-096",
+        "fill-099",
+        "fill-103",
+        "fill-108",
+        "fill-112",
+        "fill-114",
+        "fill-115",
+        "fill-122",
+        "fill-133",
+        "fill-134",
+        "fill-135",
+        "fill-140"
+    ];
+
+    var wallpaper = wallpapers[Math.floor(Math.random() * wallpapers.length)];
+    document.body.classList.add(wallpaper);
+
 });
-
-var wallpapers = [
-    "fill-071",
-    "fill-074",
-    "fill-075",
-    "fill-076",
-    "fill-080",
-    "fill-087",
-    "fill-096",
-    "fill-099",
-    "fill-103",
-    "fill-108",
-    "fill-112",
-    "fill-114",
-    "fill-115",
-    "fill-122",
-    "fill-133",
-    "fill-134",
-    "fill-135",
-    "fill-140"
-];
-
-document.body.classList.add(wallpapers[Math.floor(Math.random() * wallpapers.length)]);
