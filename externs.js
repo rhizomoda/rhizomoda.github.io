f2bf2d9b78741861a2082c661bfb9b8b4842914c
Alexandre Christian Elie Gagnon <alxgnon@gmail.com>
2023-04-30 19:31:12 -0400

    changes

--- externs.js
+++ externs.js
@@ -1,3 +1,20 @@
+var Session;
+Session.prototype.getValue = function () {};
+Session.prototype.on = function () {};
+Session.prototype.setValue = function () {};
+var Editor;
+/**
+ * @return Session
+ */
+Editor.prototype.getSession = function () {};
+var ace = {
+    define: function () {},
+    /**
+     * @return Editor
+     */
+    edit: function () {},
+    require: function () {}
+};
 /**
  * @constructor
  */
--- src/rhizomoda/core.cljs
+++ src/rhizomoda/core.cljs
@@ -1,59 +1,7 @@
 (ns rhizomoda.core)
 
-;; (defn initialize-editor [el document]
-;;   (.edit js/ace el (clj->js {:enableLinking true
-;;                              :enableMultiselect true
-;;                              :fontSize 11 
-;;                              ;;:mode "ace/mode/rhizomoda"
-;;                              :printMargin 34
-;;                              :scrollPastEnd 1
-;;                              :showLineNumbers false
-;;                              :tabSize 2
-;;                              :theme "ace/theme/ambiance"
-;;                              :useSoftTabs false
-;;                              :wrap 34})))
-
-;; (initialize-editor "primary" "index.document")
-;; (initialize-editor "secondary" "moda.document")
-;; (initialize-editor "tertiary" "rhizo.document")
-
-(def virtual-sky (.getElementById js/document "virtualsky")) 
-
-(defn toggle-virtual-sky [] (.toggle (. virtual-sky -classList) "hidden"))
-
-(def menu (.getElementById js/document "menu"))
-
-(new js/MenuBar menu
-     (clj->js [{:text "Widgets"
-                :subMenuItems [{:text "VirtualSky"
-                                :handler toggle-virtual-sky}]}]))
-;; mon cv
-;; preparer des questions
-;; je cherche des relations en personne avec mes coworkers
-
-;; ask for redacted invite
-
-;; Awareness, intention & Meditation.
-
-;; get tested
-;; go to the doctor
-
-;; get stuff from Walling and Spotify
-
-;; var wallpapers = ['fill-071',
-;;                   'fill-074',
-;;                   'fill-075',
-;;                   'fill-076',
-;;                   'fill-080',
-;;                   'fill-087',
-;;                   'fill-096',
-;;                   'fill-099',
-;;                   'fill-103',
-;;                   'fill-108',
-;;                   'fill-114',
-;;                   'fill-115'];
-;; document.body.classList.add (wallpapers [Math.floor (Math.random () * wallpapers.length)]);
-
+;; (defn initialize-editor [el, filename]
+;;   (js/ace.edit))
 
 ;; function initializeEditor(el, filename) {
 ;;   var editor = ace.edit(el, {
@@ -95,7 +43,51 @@
 ;;   }
 ;; });
 
-;; // mode-rhizomoda.js
+;; select an HTML element
+(defn query-selector [selectors]
+  (. js/document querySelector selectors))
+
+;; toggle show or hide
+(defn show-hide [selectors]
+  (-> (query-selector selectors) (.-classList) (.toggle "hidden")))
+
+;; initialize the menu bar
+(new js/MenuBar
+     (query-selector "#menu-bar")
+     (clj->js [{:text "Widgets"
+                :subMenuItems
+                [{:text "VirtualSky"
+                  :handler #(show-hide "#virtual-sky-widget")}]}]))
+
+;; background image classes from CSS
+(def wallpaper ["fill-071"
+                "fill-074"
+                "fill-075"
+                "fill-076"
+                "fill-080"
+                "fill-087"
+                "fill-096"
+                "fill-099"
+                "fill-103"
+                "fill-108"
+                "fill-114"
+                "fill-115"])
+
+;; apply a random wallpaper
+(. js/document.body.classList add (rand-nth wallpaper))
+
+;; preparer des questions
+;; je cherche des relations en personne avec mes coworkers
+
+;; ask for redacted invite
+
+;; Awareness, intention & Meditation.
+
+;; get tested
+;; go to the doctor
+
+;; get stuff from Walling and Spotify
+
 ;; ace.define(
 ;;   'ace/mode/rhizomoda',
 ;;   [
