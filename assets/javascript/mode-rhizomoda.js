e4fcb31f295efdd2f55a108c7e7be133a846b762
Alexandre Gagnon <alxgnon@gmail.com>
2023-06-06 22:10:19 -0400

    update rules

--- assets/javascript/mode-rhizomoda.js
+++ assets/javascript/mode-rhizomoda.js
@@ -1,42 +1,63 @@
 ace.define(
-  'ace/mode/rhizomoda',
+  "ace/mode/rhizomoda",
   [
-    'require',
-    'exports',
-    'ace/lib/oop',
-    'ace/mode/text_highlight_rules',
-    'ace/mode/text'
+    "require",
+    "exports",
+    "ace/lib/oop",
+    "ace/mode/text_highlight_rules",
+    "ace/mode/text"
   ],
   function (require, exports) {
-    'use strict';
-    var oop = require('../lib/oop');
+    "use strict";
+    var oop = require("../lib/oop");
 
-    function highlightRules() {
+    function HighlightRules() {
       this.$rules = {
-        'start': [{
-          token: 'keyword',
-          regex: /(IDEA|IN PROGRESS|MAYBE|NOTE|TODO|URL|WAITING FOR)[.!?,:;]+/
+        "start": [{
+          token: "keyword",
+          regex: /(IDEA|MAYBE|TODO|WAITING FOR)[.!?,:;]+/
         }, {
-          token: 'string',
-          regex: /(DONE|PROJECT|QUESTION|SECTION|SUBJECT|TOPIC)[.!?,:;]+/
+          token: "string",
+          regex: /(QUESTION|SYSTEM)[.!?,:;]+/
         }, {
-          token: 'link',
+          token: "constant",
+          regex: /(NOTE)[.!?,:;]+/
+        }, {
+          token: "support.function",
+          regex: /(IN PROGRESS)[.!?,:;]+/
+        }, {
+          token: "support.class",
+          regex: /(ITEM|LINK)[.!?,:;]+/
+        }, {
+          token: "variable.parameter",
+          regex: /(SECTION)[.!?,:;]+/
+        }, {
+          token: "constant.numeric",
+          regex: /(PROJECT)[.!?,:;]+/
+        }, {
+          token: "keyword.operator",
+          regex: /(DONE)[.!?,:;]+/
+        }, {
+          token: "support.type",
+          regex: /(TOPIC)[.!?,:;]+/
+        }, {
+          token: "link",
           regex: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
         }]
       };
       this.normalizeRules();
     };
-    oop.inherits(highlightRules, require('./text_highlight_rules').TextHighlightRules);
+    oop.inherits(HighlightRules, require("./text_highlight_rules").TextHighlightRules);
 
     function Mode() {
       this.$behaviour = this.$defaultBehaviour;
-      this.HighlightRules = highlightRules;
+      this.HighlightRules = HighlightRules;
     };
-    oop.inherits(Mode, require('./text').Mode);
+    oop.inherits(Mode, require("./text").Mode);
     (function () {
-      this.$id = 'ace/mode/rhizomoda';
+      this.$id = "ace/mode/rhizomoda";
     }).call(Mode.prototype);
     exports.Mode = Mode;
   }
 );
-ace.require(['ace/mode/rhizomoda']);
+ace.require(["ace/mode/rhizomoda"]);
